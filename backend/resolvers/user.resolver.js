d062d0b1dccb2deb676282313293c5b9e20919c6
AyushRai <raiayush572@gmail.com>
2024-06-18 01:09:02 +0530

    user resolver added

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,18 +1,109 @@
+
 import { users } from '../dummyData/data.js';
+import bcrypt from 'bcryptjs';
+import User from '../models/user.model.js';
+
+
 const userResolver = {
     Query: {
-        users:()=>{
-            return users;
+        authUser: async(_,__,context)=>{
+            try{
+                const user=await context.getUser();
+                return user;
+            }
+            catch(error){
+                console.log("error in authUser",error);
+                throw new Error(error.message || "Auth user problem");
+            }    
+        
+        
+        },
 
-        },
-        user:(_,{userId})=>{
-            return users.find(user=>user._id===userId);
-        },
+        user: async(_,{userId})=>{
+            try{
+                const user=await User.findById(userId);
+                return user;
+            }
+            catch(error){
+                console.log("error in user finding",error);
+                throw new Error(error.message || "error getting user");
+            }
+        }
+
+
+       
 
 
 
 
     },
-    Mutation: {}
+    Mutation: {
+        signUp: async (_,{input},context)=>{
+            try{
+                const {username,name,password,gender}=input;
+                if(!username|| !name || !password || !gender){
+                    throw new Error("Please fill all the fields");
+                }
+                const existingUser=User.findOne({username});
+                if(existingUser){
+                    throw new Error("User already exists");
+                }
+                const salt=await bcrypt.genSalt(10);
+                const hashedPassword=await bcrypt.hash(password,salt);
+                const boyProfilePic=`https://avatar.iran.liara.run/public/boy?username=${username}`;
+                const girlProfilePic=`https://avatar.iran.liara.run/public/girl?username=${username}`;
+                const newUser=new User({
+                    username,
+                    name,
+                    password:hashedPassword,
+                    gender,
+                    profilePicture:gender==="male"?boyProfilePic:girlProfilePic
+                });
+                await newUser.save();
+                await context.login(newUser);
+                return newUser;
+
+
+            }
+            catch(error){
+                console.log("error in sign up",error);
+                throw new Error(error.message || "internal server error");
+            }
+        },
+
+        login: async (_,{input},context)=>{
+            try{
+                const {username,password}=input;
+                if(!username|| !password){
+                    throw new Error("Please fill all the fields");
+                }
+                const user= await context.authenticate("graphql-local",{username,password});
+                await context.login(user);
+                return user;    
+            }
+            catch(error){
+                console.log("error in login",error);
+                throw new Error(error.message || "internal server error");
+            }
+        },
+
+        logout: async(_,__,context)=>{
+            try{
+                await context.logout();
+                req.session.destroy((err)=>{
+                    if(err){
+                        throw err;
+                    }
+                });
+                res.clearCookie("connect.sid");
+                return {message:"Logged out successfully"};
+            }
+            catch(error){
+                console.log("error in logout",error);
+                throw new Error(error.message || "internal server error");
+            }
+        }
+
+    }
 };
 export default userResolver;
--- backend/typeDefs/user.typeDef.js
+++ backend/typeDefs/user.typeDef.js
@@ -9,7 +9,7 @@ const userTypeDef=`#graphql
     }
 
     type Query{
-        users:[User!]
+        
         authUser: User
         user(userId: ID!): User
 
