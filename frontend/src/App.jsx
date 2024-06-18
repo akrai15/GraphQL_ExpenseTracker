84bc291a78447262916cbf24951791edc22e5906
AyushRai <raiayush572@gmail.com>
2024-06-18 20:09:14 +0530

    UI design added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import TransactionPage from './pages/TransactionPage'
+import NotFoundPage from './pages/NotFoundPage'
+import Header from './components/ui/Header'
+
+
+
+
+
+function App() {
+  const authUser = false
+
+  return (  
+    <>
+     {authUser && <Header/>}
+     <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/signup" element={<SignUpPage/>} />
+      <Route path="/transaction/:id" element={<TransactionPage/>} />
+      <Route path="*" element={<NotFoundPage/>} />
+
+
+
+     </Routes>
+      
+    </>
+  )
+}
+
+export default App
